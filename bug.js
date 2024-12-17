3e6845b1feb85d2a268060eb06f8fe50755e5636
BugHunter-X <askzenai@gmail.com>
2024-12-17 08:36:07 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+app.get('/', (req, res) => {
+  // Simulate an asynchronous operation that might throw an error
+  someAsyncOperation().then(() => {
+    res.send('Hello World!');
+  }).catch(err => {
+    // This error will not be handled properly, leading to an unhandled promise rejection
+    console.error('Error:', err);
+  });
+});
+
+async function someAsyncOperation() {
+  // Simulate an asynchronous operation that might throw an error
+  await new Promise((resolve, reject) => {
+    setTimeout(() => {
+      // Simulate a random error
+      if (Math.random() < 0.5) {
+        reject(new Error('Something went wrong!'));
+      } else {
+        resolve();
+      }
+    }, 1000);
+  });
+}
